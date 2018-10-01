84523f6809c3aa2f2158d9088e40e559670b95e6
Owen Xie <owenxie24@gmail.com>
2018-10-01 19:06:05 -0400

    DO NOT MERGE YET - RESULTS ARE CURRENTLY BROKEN
    For two objects, the function is returning more plausible worlds, but I'm sure this is incorrect.
    
    In addition, the time has decreased even though this should take a longer time since we've only tested 2 object worlds so far (explaining the lack of change in call to location_check count) (Your function should have run faster with 2 objects because there's less overhead).

--- Tarskis World Version 2.c
+++ Tarskis World Version 2.c
@@ -26,6 +26,78 @@ bool letter_check(uint32_t sizeof_w, uint32_t* w)
 	return true;
 }
 
+bool location_check_v2(uint32_t sizeof_w, uint32_t* w)
+{
+    // World representation: Each column is two bits (reversed), each row is each element in the array
+    // Please note the the right side is x == 0, leftside is x == 7
+    // If the point has 0, nothing occupies it
+    // If the point has 1, a center occupies it
+    // If the point has 2, a edge of a large object occupies it
+    uint16_t world[8] = {0,0,0,0,0,0,0,0};
+    
+    for(uint32_t i = 0; i < sizeof_w; i++)
+    {
+	uint8_t c = (w[i] >> 6) & 63;
+	
+	uint8_t cy = c & 7;
+	uint8_t cx = (c >> 2) & 14; // We want this to be multiplied by 2 - really the shift left
+
+	// Y position above and below the center
+	uint8_t by = cy + 1;
+	uint8_t ty = cy - 1;
+	
+	uint8_t large = (w[i] >> 15) & 1;
+
+	// Placement of object
+	if((world[cy] >> cx) & 3) { // If center is occupied by 1 or 2
+	    return false;
+	}
+	if(large) { // Bound search
+	    uint16_t e_bit =  (2 << cx);
+	    if(cx != 14) { // Check "left" side
+		if((world[cy] >> cx) & 4) return false; // If space if occupied by a center
+		else world[cy] = world[cy] | (e_bit << 2);
+	    }
+	    if(cx) { // Check "right" side - x >= 2
+		if((world[cy] >> (cx - 2)) & 1) return false; // If space if occupied by a center
+		else world[cy] = world[cy] | (e_bit >> 2);
+	    }
+
+	    if(cy != 7) { // Check bottom
+		if((world[by] >> cx) & 1) return false;
+		else world[by] = world[by] | e_bit;
+	    }
+	    if(cy) { // Check top (y != 0)
+		if((world[ty] >> cx) & 1) return false;
+		else world[ty] = world[ty] | e_bit;
+	    }
+
+	    // Edge cases
+	    if(cx != 14 && cy) { // Check "left" and up side
+		if((world[ty] >> cx) & 4) return false; // If space if occupied by a center
+		else world[ty] = world[ty] | (e_bit << 2);
+	    }
+
+	    if(cx && cy) { // Check "right" and up side - x >= 2
+		if((world[ty] >> (cx - 2)) & 1) return false; // If space if occupied by a center
+		else world[ty] = world[ty] | (e_bit >> 2);
+	    }
+
+	    if(cx != 14 && cy != 7) { // Check "left" and bottom side
+		if((world[by] >> cx) & 4) return false; // If space if occupied by a center
+		else world[by] = world[by] | (e_bit << 2);
+	    }
+
+	    if(cx && cy != 7) { // Check "right" and bottom side - x >= 2
+		if((world[by] >> (cx - 2)) & 1) return false; // If space if occupied by a center
+		else world[by] = world[by] | (e_bit >> 2);
+	    }	    
+	}
+    }
+    
+    return true;
+}
+
 bool location_check(uint32_t l_[])
 {
 	int c0c = (l_[0] & 4032) >> 6;
@@ -84,10 +156,10 @@ int check_world(uint32_t w[], int sizeof_w)
 	if(!letter_check(sizeof_w, w))
 	    return 0;
 	
-	if(!(location_check(temp_pair)))
+	if(!(location_check_v2(sizeof_w, w)))
 		return 0;
 	
-	while(true)
+	/*while(true)
 	{
 		bool successful_loop = true;
 
@@ -115,7 +187,7 @@ int check_world(uint32_t w[], int sizeof_w)
 
 		if(!(location_check(temp_pair_2)))
 			return 0;
-	}
+	}*/
 
 	return 1;
 }
@@ -182,56 +254,56 @@ void test_cases()
 	// 1) should fail
 	idk[0] = 1 << 6;
 	idk[1] = 1 << 6;
-	assert(!(location_check(idk)));
-	printf("%d\n",location_check(idk));
+	assert(!(location_check_v2(2, idk)));
+	printf("%d\n",location_check_v2(2, idk));
 	
 	// 2) should fail
 	idk[0] = 8 << 6;
 	idk[1] = 8 << 6;
-	assert(!(location_check(idk)));
-	printf("%d\n",location_check(idk));
+	assert(!(location_check_v2(2, idk)));
+	printf("%d\n",location_check_v2(2, idk));
 	
 	// 3) should pass
 	idk[0] = 1 << 15;
 	idk[1] = 33 << 10;
-	assert(location_check(idk));
-	printf("3 -- %d\n",location_check(idk));
+	assert(location_check_v2(2, idk));
+	printf("3 -- %d\n",location_check_v2(2, idk));
 	
 	// 4) should fail
 	idk[0] = 1 << 15;
 	idk[1] = 65 << 9;
-	assert(!(location_check(idk)));
-	printf("4 -- %d\n",location_check(idk));
+	assert(!(location_check_v2(2, idk)));
+	printf("4 -- %d\n",location_check_v2(2, idk));
 	
 	// 5) should fail
 	idk[0] = 1 << 15;
 	idk[1] = ((1<<7)|1) << 9;
-	assert(!(location_check(idk)));
-	printf("%d\n",location_check(idk));
+	assert(!(location_check_v2(2, idk)));
+	printf("%d\n",location_check_v2(2, idk));
 	
 	// 6) should pass
 	idk[0] = 1 << 15;
 	idk[1] = ((1<<6)|1) << 10;
-	assert(location_check(idk));
-	printf("%d\n",location_check(idk));
+	assert(location_check_v2(2, idk));
+	printf("%d\n",location_check_v2(2, idk));
 	
 	/*
 	// 512 & 514 should pass
 	idk[0] = 512;
 	idk[1] = 514;
-	assert(location_check(idk));
-	printf("%d\n",location_check(idk));
+	assert(location_check_v2(2, idk));
+	printf("%d\n",location_check_v2(2, idk));
 	
 	
 	// 1024 & 528 should pass
 	idk[0] = 1024;
 	idk[1] = 528;
-	assert(location_check(idk));
-	printf("%d\n",location_check(idk));
+	assert(location_check_v2(2, idk));
+	printf("%d\n",location_check_v2(2, idk));
 	*/
 	
 	assert(0);
-	//location_check(idk));
+	//location_check_v2(2, idk));
 }
 
 void valid_objects_tests(uint32_t v[])
